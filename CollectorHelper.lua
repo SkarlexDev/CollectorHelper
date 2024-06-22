ab337331c8fefb7e2a9631ed8d3558a779a91cad
SkarlexDev <luciadi771@yahoo.com>
2024-06-22 18:51:42 +0300

    Added merchant required total currency/page

--- CollectorHelper.lua
+++ CollectorHelper.lua
@@ -1,3 +1,28 @@
+-- init
+local merchantFrameCost = CreateFrame("Frame", "CollectorHelper_MerchantButton", MerchantFrame, "BackdropTemplate")
+merchantFrameCost:SetWidth(250)
+merchantFrameCost:SetHeight(MerchantFrame:GetHeight())
+merchantFrameCost:SetPoint("TOPRIGHT", 250, 0)
+
+merchantFrameCost:SetBackdrop({
+  bgFile = "Interface\\Buttons\\WHITE8x8",
+  edgeFile = "Interface\\Buttons\\WHITE8x8",
+  tileEdge = false,
+  edgeSize = 1,
+  insets = { left = 1, right = 1, top = 1, bottom = 1 },
+})
+merchantFrameCost:SetBackdropColor(0.05, 0.05, 0.05, 0.95)
+merchantFrameCost:SetBackdropBorderColor(0, 0, 0, 1)
+
+local merchantCostTitle = merchantFrameCost:CreateFontString(nil, "OVERLAY", "GameTooltipText")
+merchantCostTitle:SetPoint("TOP",0,-8)
+merchantCostTitle:SetText("\124cnYELLOW_FONT_COLOR:Currency Needed to Collect this page")
+
+
+local marchantCost = merchantFrameCost:CreateFontString(nil, "OVERLAY", "GameTooltipText")
+marchantCost:SetPoint("TOPLEFT",35,-40)
+
+-- addon logic
 -- check Miscellaneous (toys/mounts)
 local function checkMiscellaneousOwned(itemId)
   local isToy = C_ToyBox.GetToyInfo(itemId) ~= nil
@@ -39,36 +64,52 @@ local function checkShopID(itemId,iTypo)
   elseif itemSetId ~= nil then
     return setOwned(itemSetId)
   elseif isItemHeirloom then
-    return C_Heirloom.PlayerHasHeirloom(itemId)  
+    return C_Heirloom.PlayerHasHeirloom(itemId)
   else
     return C_TransmogCollection.PlayerHasTransmog(itemId)
   end
 end
 
 
-local function isReal(frame) return frame ~= nil and frame:IsShown() end
-
 local function updateShop()
   -- great point to block addon if not settings.hideMerchant then return end
-  local size = 10
-  if isReal(MerchantItem11) then
-    while isReal(_G["MerchantItem"..(size + 1)]) do size = size + 1 end
-  end
-  for i = 1,size do
-    local itemId = GetMerchantItemID((MerchantFrame.page - 1) * size + i)
+  local size = MERCHANT_ITEMS_PER_PAGE
+  local currencyMap = {}
+    for i = 1,size do
+    local itemIndex = (MerchantFrame.page - 1) * size + i
+    local itemId = GetMerchantItemID(itemIndex)
     if itemId ~= nil then
-      local _,_,_,_,_,iTypo,_,_,_,_,_,_ = C_Item.GetItemInfo(itemId) 
-      -- Get All ids from merchant
-      --print(C_Item.GetItemLearnTransmogSet(itemId))
-      --print(itemId)      
+      local _,_,_,_,_,iTypo,_,_,_,_,_,_ = C_Item.GetItemInfo(itemId)
       if checkShopID(itemId,iTypo) then
         SetItemButtonSlotVertexColor(_G["MerchantItem"..i],0.4,0.4,0.4)
         _G["MerchantItem"..i.."ItemButton"]:Hide()
-        _G["MerchantItem"..i.."Name"]:SetText("")      
+        _G["MerchantItem"..i.."Name"]:SetText("")
         _G["MerchantItem"..i.."AltCurrencyFrame"]:Hide()
+      else
+        local currencyIndex = GetMerchantItemCostInfo(itemIndex)
+        for y = 1,currencyIndex do
+          local itemTexture, itemValue, itemLink, currencyName = GetMerchantItemCostItem(itemIndex,y)
+          if itemTexture then
+            if currencyMap[itemTexture] then
+              currencyMap[itemTexture] = currencyMap[itemTexture] + itemValue
+            else
+              currencyMap[itemTexture] = itemValue
+            end
+          end
+        end
       end
     end
   end
+
+  local cost = "";
+  for itemTexture, totalValue in pairs(currencyMap) do
+    cost = cost .. "|T"..itemTexture..":16|t "..totalValue.. "\n\n"
+  end
+
+  if cost == "" then
+    cost = "\124cnPURE_GREEN_COLOR:You have everything on this page"
+  end
+  marchantCost:SetText(cost)
 end
 
 
